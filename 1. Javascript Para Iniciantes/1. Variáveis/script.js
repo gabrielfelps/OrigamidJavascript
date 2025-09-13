2b1118c493c441943d012e1a8561a95ba77ed375
gabrielfelps <ugabrielfelipe@icloud.com>
2025-09-12 23:18:50 -0300

    feat: Adiciona o capítulo Javascript Para Iniciantes com o primeiro exercício.

---
+++ 1. Javascript Para Iniciantes/1. Variáveis/script.js
@@ -0,0 +1,18 @@
+// Declarar uma variável com o seu nome
+const nome = "Gabriel";
+
+// Declarar uma variável com a sua idade
+const idade = 23;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor.
+let comida;
+
+// Atribuir valor a sua comida favorita
+comida = Hamburger;
+
+// Declarar 5 variáveis diferentes sem valores
+let marca;
+let veiculo;
+let nota;
+let cpf;
+let peçaDeRoupa;
