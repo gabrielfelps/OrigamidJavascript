ce6de0543434877993ff0ca582c7ee04f507f838
gabrielfelps <ugabrielfelipe@icloud.com>
2025-09-12 23:25:49 -0300

    feat: Adiciona o segundo exercício.

---
+++ 1. Javascript Para Iniciantes/2. Numeros e Operadores/script.js
@@ -0,0 +1,22 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + (5 * 2) / 2 + 20; // Resultado: 35
+
+// Crie duas expressões que retornem NaN
+const expressao1 = "Isso é um teste.";
+
+// Somar a string '200' com o número 50 e retornar 250
+const numero200 = "200";
+const soma = +numero200 + 50;
+console.log(soma);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+let numero5 = 5;
+numero5++;
+console.log(numero5);
+
+// Como dividir o peso por 2?
+var numero = "80";
+var unidade = "kg";
+var peso = numero; // '80kg'
+var pesoPorDois = peso / 2 + unidade; // NaN (Not a Number)
+console.log(pesoPorDois);
