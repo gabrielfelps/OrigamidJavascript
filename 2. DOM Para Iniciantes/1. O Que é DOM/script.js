f74a684b8f987cbd9d4027982fe8a4349d63577e
gabrielfelps <ugabrielfelipe@icloud.com>
2025-09-14 14:20:21 -0300

    feat: Adiciona o capítulo "DOM Para Iniciantes" com o primeiro exercício.

---
+++ 2. DOM Para Iniciantes/1. O Que é DOM/script.js
@@ -0,0 +1,11 @@
+// Retorne o url da página atual utilizando o objeto window.
+const url = location.href;
+
+// Seleciona o primeiro elemento da página que possua a classe ativo.
+const btn = document.querySelector(".ativo");
+
+// Retorne a linguagem do navegador.
+const linguagem = navigator.language;
+
+// Retorne a largura da janela.
+const larguraJanela = window.innerWidth;
