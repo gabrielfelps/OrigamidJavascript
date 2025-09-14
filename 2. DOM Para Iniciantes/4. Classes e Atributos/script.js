3b3619b5e47e6b0fa0755c7870aa72f6b87c9d77
gabrielfelps <ugabrielfelipe@icloud.com>
2025-09-14 15:01:06 -0300

    feat: Adiciona o quarto exercício

---
+++ 2. DOM Para Iniciantes/4. Classes e Atributos/script.js
@@ -0,0 +1,27 @@
+// Adicione a classe ativo a todos os itens do menu
+const itensMenu = document.querySelectorAll(".menu li");
+
+itensMenu.forEach((item) => {
+  item.classList.add("ativo");
+});
+
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+itensMenu.forEach((item, index) => {
+  if (index === 0) {
+    item.classList.add("ativo");
+  } else {
+    item.classList.remove("ativo");
+  }
+});
+
+// Verifique se as imagens possuem o atributo alt
+const imgs = document.querySelectorAll("img");
+
+imgs.forEach((img, index) => {
+  console.log(img.hasAttribute("alt"), index);
+});
+
+// Modifique o href do link externo no menu
+const linkExterno = (document.querySelector("[href^='https://']").innerText =
+  "Modificado.");
+console.log(linkExterno);
