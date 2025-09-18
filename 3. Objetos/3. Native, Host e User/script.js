4d0a40ca790659ed3835288038685b2a66aa5078
gabrielfelps <ugabrielfelipe@icloud.com>
2025-09-18 11:27:33 -0300

    feat: Adiciona o terceiro exercício

---
+++ 3. Objetos/3. Native, Host e User/script.js
@@ -0,0 +1,17 @@
+// Liste 5 objetos nativos
+Object;
+String;
+Function;
+Array;
+Boolean;
+
+// Liste 5 objetos do browser
+Window;
+Document;
+HTMLAllCollection;
+NodeList;
+History;
+
+// Liste 2 Métodos, Propriedades ou Objetos presentes no Chrome que não existem no Firefox
+webkitVisibilityState;
+webkitHidden;
