de161f45c7af5a02336395d2c274b73803c05911
gabrielfelps <ugabrielfelipe@icloud.com>
2025-09-25 12:32:40 -0300

    feat: Adiciona o terceiro exercício

---
+++ 4. Efeitos no Dom/3. Modal/scripts/script.js
@@ -0,0 +1,11 @@
+import initAnimaScroll from "./modules/animacao-scroll.js";
+import initSmoothScroll from "./modules/scroll-suave.js";
+import initAccordionList from "./modules/accordion-list.js";
+import initTabContent from "./modules/tab-content.js";
+import initModal from "./modules/modal.js";
+
+initAnimaScroll();
+initSmoothScroll();
+initAccordionList();
+initTabContent();
+initModal();
