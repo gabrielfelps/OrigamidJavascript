67718559cfa7f30c2d51f7bb69d1803d5763d2ea
gabrielfelps <ugabrielfelipe@icloud.com>
2025-09-28 13:12:33 -0300

    feat: Adiciona o quarto exercício

---
+++ 4. Efeitos no Dom/4. Tooltip/scripts/script.js
@@ -0,0 +1,13 @@
+import initAnimaScroll from "./modules/animacao-scroll.js";
+import initSmoothScroll from "./modules/scroll-suave.js";
+import initAccordionList from "./modules/accordion-list.js";
+import initTabContent from "./modules/tab-content.js";
+import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
+
+initAnimaScroll();
+initSmoothScroll();
+initAccordionList();
+initTabContent();
+initModal();
+initToolTip();
