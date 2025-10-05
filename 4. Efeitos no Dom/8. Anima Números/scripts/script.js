c1721bb75b10503791cd8c3a3b0727c6dd5e6755
gabrielfelps <ugabrielfelipe@icloud.com>
2025-10-05 14:36:58 -0300

    feat: Adiciona o oitavo exercício

---
+++ 4. Efeitos no Dom/8. Anima Números/scripts/script.js
@@ -0,0 +1,19 @@
+import initAnimaScroll from "./modules/animacao-scroll.js";
+import initSmoothScroll from "./modules/scroll-suave.js";
+import initAccordionList from "./modules/accordion-list.js";
+import initTabContent from "./modules/tab-content.js";
+import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
+
+initAnimaScroll();
+initSmoothScroll();
+initAccordionList();
+initTabContent();
+initModal();
+initToolTip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
