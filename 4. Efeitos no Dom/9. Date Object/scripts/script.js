a27e671bf1467575fc09ef5a520ad94e295d22a3
gabrielfelps <ugabrielfelipe@icloud.com>
2025-10-08 14:16:55 -0300

    feat: Adiciona o nono exercício que foi pulado em "Efeitos no DOM"

---
+++ 4. Efeitos no Dom/9. Date Object/scripts/script.js
@@ -0,0 +1,21 @@
+import initAnimaScroll from "./modules/animacao-scroll.js";
+import initSmoothScroll from "./modules/scroll-suave.js";
+import initAccordionList from "./modules/accordion-list.js";
+import initTabContent from "./modules/tab-content.js";
+import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
+import initFuncionamento from "./modules/funcionamento.js";
+
+initAnimaScroll();
+initSmoothScroll();
+initAccordionList();
+initTabContent();
+initModal();
+initToolTip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
