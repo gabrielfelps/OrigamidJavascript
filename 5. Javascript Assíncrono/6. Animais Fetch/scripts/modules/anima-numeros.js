b976e7f34602320c0be30f34a61d58234e56d3be
gabrielfelps <ugabrielfelipe@icloud.com>
2025-10-17 14:19:33 -0300

    feat: Adiciona o sexto exercício

---
+++ 5. Javascript Assíncrono/6. Animais Fetch/scripts/modules/anima-numeros.js
@@ -0,0 +1,33 @@
+export default function initAnimaNumeros() {
+  const numeros = document.querySelectorAll("[data-numero]");
+
+  function animaNumeros() {
+    numeros.forEach((numero) => {
+      let contador = 0;
+      const total = Number(numero.innerText);
+      const incremento = Math.floor(total / 100);
+
+      const timer = setInterval(() => {
+        contador += incremento;
+        numero.innerText = contador;
+
+        if (contador > total) {
+          numero.innerText = total;
+          clearInterval(timer);
+        }
+      }, 25 * Math.random());
+    });
+  }
+
+  function handleMutation(mutation) {
+    if (mutation[0].target.classList.contains("active")) {
+      mutationObserver.disconnect();
+      animaNumeros();
+    }
+  }
+
+  const observeTarget = document.querySelector(".numeros");
+
+  const mutationObserver = new MutationObserver(handleMutation);
+  mutationObserver.observe(observeTarget, { attributes: true });
+}
---
+++ 5. Javascript Assíncrono/6. Animais Fetch/scripts/script.js
@@ -0,0 +1,21 @@
+import initAnimaScroll from "./modules/animacao-scroll.js";
+import initSmoothScroll from "./modules/scroll-suave.js";
+import initAccordionList from "./modules/accordion-list.js";
+import initTabContent from "./modules/tab-content.js";
+import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
+import initFuncionamento from "./modules/funcionamento.js";
+
+initAnimaScroll();
+initSmoothScroll();
+initAccordionList();
+initTabContent();
+initModal();
+initToolTip();
+initDropdownMenu();
+initMenuMobile();
+initFetchAnimais();
+initFuncionamento();
