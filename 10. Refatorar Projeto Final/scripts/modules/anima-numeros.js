9c1f6babfa889f58685e38f60f56fb14c3cd84df
gabrielfelps <gabrielfelps.dev@gmail.com>
2025-11-23 15:36:06 -0300

    refatorar: Refatora o Anima Numeros
    O arquivo anima-numeros.js foi reformulado para que a função exportada ceda lugar para uma classe.

--- 10. Refatorar Projeto Final/scripts/modules/anima-numeros.js
+++ 10. Refatorar Projeto Final/scripts/modules/anima-numeros.js
@@ -1,33 +1,56 @@
-export default function initAnimaNumeros() {
-  const numeros = document.querySelectorAll('[data-numero]');
+export default class AnimaNumeros {
+  constructor(numeros, observeTarget, observerClass) {
+    this.numeros = document.querySelectorAll(numeros);
+    this.observeTarget = document.querySelector(observeTarget);
+    this.observerClass = observerClass;
 
-  function animaNumeros() {
-    numeros.forEach((numero) => {
-      let contador = 0;
-      const total = Number(numero.innerText);
-      const incremento = Math.floor(total / 100);
+    // bind o this do objeto ao callback da mutação
+    this.handleMutation = this.handleMutation.bind(this);
+  }
 
-      const timer = setInterval(() => {
-        contador += incremento;
-        numero.innerText = contador;
+  // Recebe um elemento do DOM, com número como texto. Este método irá incrementar a partir de 0 até o número final.
+  static incrementarNumero(numero) {
+    let contador = 0;
+    const total = Number(numero.innerText);
+    const incremento = Math.floor(total / 100);
 
-        if (contador > total) {
-          numero.innerText = total;
-          clearInterval(timer);
-        }
-      }, 25 * Math.random());
+    const timer = setInterval(() => {
+      contador += incremento;
+      numero.innerText = contador;
+
+      if (contador > total) {
+        numero.innerText = total;
+        clearInterval(timer);
+      }
+    }, 25 * Math.random());
+  }
+
+  // Ativa incrementarNumero para cada número selecionado dentro da NodeList.
+  animaNumeros() {
+    this.numeros.forEach((numero) => {
+      this.constructor.incrementarNumero(numero);
     });
   }
 
-  function handleMutation(mutation) {
-    if (mutation[0].target.classList.contains('active')) {
-      mutationObserver.disconnect();
-      animaNumeros();
+  // É chamado quando a mutação ocorrer.
+  handleMutation(mutation) {
+    if (mutation[0].target.classList.contains(this.observerClass)) {
+      this.oberver.disconnect();
+      this.animaNumeros();
     }
   }
 
-  const observeTarget = document.querySelector('.numeros');
+  // Adiciona o MutationObserver para verificar quando a classe 'active' é adicionada ao elemento-alvo.
+  addMutationObserver() {
+    this.oberver = new MutationObserver(this.handleMutation);
+    this.oberver.observe(this.observeTarget, { attributes: true });
+  }
 
-  const mutationObserver = new MutationObserver(handleMutation);
-  mutationObserver.observe(observeTarget, { attributes: true });
+  init() {
+    if (this.numeros.length && this.observeTarget) {
+      this.addMutationObserver();
+    }
+
+    return this;
+  }
 }
