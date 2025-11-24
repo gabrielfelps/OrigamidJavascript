54acc45d9292b11e002ed84852646fc6899e485a
gabrielfelps <gabrielfelps.dev@gmail.com>
2025-11-24 12:34:15 -0300

    refatorar: Refatora a Fetch dos Animais
    A função principal do arquivo fetch-animais.js recebe agora dois argumentos.
    - Argumento 1: Responsável por receber a URL do website(nesse caso, estamos utilizando um arquivo .json próprio).
    - Argumento 2: Responsável por definir o alvo. O mesmo elemento que receberá as informações vindas da URL.

--- 10. Refatorar Projeto Final/scripts/modules/fetch-animais.js
+++ 10. Refatorar Projeto Final/scripts/modules/fetch-animais.js
@@ -1,28 +1,37 @@
 import AnimaNumeros from './anima-numeros.js';
 
-export default function initFetchAnimais() {
-  async function fetchAnimais(url) {
+export default function fetchAnimais(url, target) {
+  // Puxa os animais através de um arquivo JSON. E cria cada animal utilizando createAnimal.
+  async function criarAnimais() {
     try {
       const animaisResponse = await fetch(url);
       const animaisJSON = await animaisResponse.json();
-      const numerosGrid = document.querySelector('.numeros-grid');
 
-      animaisJSON.forEach((animal) => {
-        const divAnimal = createAnimal(animal);
-        numerosGrid.appendChild(divAnimal);
-      });
-
-      const animaNumeros = new AnimaNumeros(
-        '[data-numero]',
-        '.numeros',
-        'active'
-      );
-      animaNumeros.init();
+      animaisJSON.forEach((animal) => preencherAnimais(animal));
+      animaAnimaisNumero();
     } catch (error) {
       console.error(error);
     }
   }
 
+  // Anima os números de cada animal.
+  function animaAnimaisNumero() {
+    const animaNumeros = new AnimaNumeros(
+      '[data-numero]',
+      '.numeros',
+      'active'
+    );
+    animaNumeros.init();
+  }
+
+  // Preenche cada animal no DOM.
+  function preencherAnimais(animal) {
+    const divAnimal = createAnimal(animal);
+    const numerosGrid = document.querySelector(target);
+    numerosGrid.appendChild(divAnimal);
+  }
+
+  // Cria a div contendo informações dos animais.
   function createAnimal(animal) {
     const div = document.createElement('div');
     div.classList.add('numero-animal');
@@ -31,5 +40,5 @@ export default function initFetchAnimais() {
     return div;
   }
 
-  fetchAnimais('./animaisapi.json');
+  return criarAnimais();
 }
