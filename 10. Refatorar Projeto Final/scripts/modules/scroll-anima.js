d2e8bd4e543d80cc01c43a2d6cf7239ce6ffee59
gabrielfelps <gabrielfelps.dev@gmail.com>
2025-11-25 13:44:04 -0300

    feat: Adiciona novas funcionalidades a classe Scroll anima
    - Adicionado função para obter a distância de cada item em relação ao topo do website.
    - Adicionado função que verifica a distância em cada objeto em relação ao scroll do website.

--- 10. Refatorar Projeto Final/scripts/modules/scroll-anima.js
+++ 10. Refatorar Projeto Final/scripts/modules/scroll-anima.js
@@ -3,24 +3,42 @@ export default class ScrollAnima {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.9;
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.windowMetade < 0;
-      if (isSectionVisible) {
-        section.classList.add('active');
-      } else if (section.classList.contains('active')) {
-        section.classList.remove('active');
+  // Obtem a distância de cada item em relação ao topo do website.
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  // Verifica a distância em cada objeto em relação ao scroll do website.
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('active');
+      } else if (item.element.classList.contains('active')) {
+        item.element.classList.remove('active');
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
-    return ScrollAnima;
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+
+  // Remove o eventListener para que a página pare de animar ao scroll.
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
