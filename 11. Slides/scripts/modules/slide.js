ae5b1c814b3cb0bab8735045cdb872b532483f03
gabrielfelps <gabrielfelps.dev@gmail.com>
2025-11-26 15:16:33 -0300

    feat: Adiciona a Movimentação do Slide
    - Criado um objeto geral dentro do Construtor.
    O objetivo com esse objeto é que ele mantenha salvo a posição inicial, as distâncias percorridas com o slide, e sua posição final.
    
    Iremos manipular o seu valor através das funções:
    > onStart()
    Nela iremos pegar o local que o usuário clicou e usarmos para saber o ínicio (ponto A - quando o usuário clica e segura).
    > updatePosition()
    Nela iremos calcular o movimento feito com o mouse, utilizando a propriedade movement do nosso objeto geral(dist). Utilizaremos o valor do clientX passado como argumento para subtrair do valor da posição incial (ponto A). Resultando na posição final do Slide (ponto B).
    Aqui também multiplicaremos esse valor para que o slide seja movido de maneira mais rápida.
    > moveSlide()
    Nela iremos mover o slide em si, alterando o estilo do transform. Também salva a posição que o slide ficou. Para que quando for mexido novamente no slide, ele começar a animação de onde parou.
    > onMove()
    Nela iremos chamar a função updatePosition(), passando para ela o clientX do event do mouseMove. E também chamaremos a função moveSlide().
    > onEnd()
    Quando o usuário solta o mouse, essa função irá salvar o valor da posição final deixada no slide.
    
    - remove os comentários.

--- 11. Slides/scripts/modules/slide.js
+++ 11. Slides/scripts/modules/slide.js
@@ -2,41 +2,49 @@ export default class Slide {
   constructor(wrapper, slide) {
     this.wrapper = document.getElementById(wrapper);
     this.slide = document.getElementById(slide);
+
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 };
+    this.scrollSensitivity = 1.4;
   }
 
-  // Será chamada quando ocorrer o evento de pressionar o botão do mouse.
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3D(${distX}px, 0, 0)`;
+  }
+
+  udpatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * this.scrollSensitivity;
+    return this.dist.finalPosition - this.dist.movement;
+  }
+
   onStart(e) {
     e.preventDefault();
-    // Evento que ocorre ao mover o mouse mouse.
+    this.dist.startX = e.clientX;
     this.wrapper.addEventListener('mousemove', this.onMove);
   }
 
-  // Será chamada quando ocorrer o evento de mover o mouse APENAS enquanto estiver com o botão do mesmo clicado.
-  onMove(e) {}
-
-  // Será chamada quando ocorrer o evento de pressionar o botão do mouse.
-  onEnd(e) {
-    this.wrapper.removeEventListener('mousemove', this.onMove);
+  onMove(e) {
+    const finalPosition = this.udpatePosition(e.clientX);
+    this.moveSlide(finalPosition);
   }
 
-  // Serão adicionados os eventos abaixo ao wrapper.
+  onEnd(e) {
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
+  }
+
   addSlideEvents() {
-    // Evento que ocorre ao pressionar o botão do mouse.
     this.wrapper.addEventListener('mousedown', this.onStart);
-    // Evento que ocorre ao soltar o botão do mouse.
     this.wrapper.addEventListener('mouseup', this.onEnd);
   }
 
-  // Todas as funções que serão passadas como callback terão o this (referente à classe) vinculado a elas.
   bindEvents() {
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
   }
 
-  // Ao iniciar a classe...
   init() {
-    // Serão chamadas as funções abaixo.
     this.bindEvents();
     this.addSlideEvents();
 
